9ace3fc08488814ac5be376f0cf8a649b27a91df
geekersnow <kaishiaini1314@126.com>
2017-06-04 16:08:51 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,8 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your cade here
+  var num = '', sum = 0;
+  num = str.split(''); 
+  for(var i=0; i< num.length;i++)
+	sum += parseInt(num[i], 10);
+  return sum;
 };
